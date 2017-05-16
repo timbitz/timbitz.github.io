8831e65ef6458c51153cc3f999fe62d42dc34c1a
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-05-16 13:18:23 -0700

    try to keep rows of 2

--- tanzania/assets/js/main.js
+++ tanzania/assets/js/main.js
@@ -252,42 +252,6 @@
 
 				});
       
-      // Thumbs.
-      $main.children('.doublethumb').each(function() {
-                                    
-                                    var	$this = $(this),
-                                    $image = $this.find('.image'), $image_img = $image.children('img'),
-                                    x;
-                                    
-                                    // No image? Bail.
-                                    if ($image.length == 0)
-                                    return;
-                                    
-                                    // Image.
-                                    // This sets the background of the "image" <span> to the image pointed to by its child
-                                    // <img> (which is then hidden). Gives us way more flexibility.
-                                    
-                                    // Set background.
-                                    $image.css('background-image', 'url(' + $image_img.attr('src') + ')');
-                                    
-                                    // Set background position.
-                                    if (x = $image_img.data('position'))
-                                    $image.css('background-position', x);
-                                    
-                                    // Hide original img.
-                                    $image_img.hide();
-                                    
-                                    // Hack: IE<11 doesn't support pointer-events, which means clicks to our image never
-                                    // land as they're blocked by the thumbnail's caption overlay gradient. This just forces
-                                    // the click through to the image.
-                                    if (skel.vars.IEVersion < 11)
-                                    $this
-                                    .css('cursor', 'pointer')
-                                    .on('click', function() {
-                                        $image.trigger('click');
-                                        });
-                                    
-                                    });
       
       
       // Poptrox.
